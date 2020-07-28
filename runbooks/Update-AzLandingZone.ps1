b159d3afa641085cad4ab7d3d33da71b14d5af39
devops <devops@acp4eu074hotmail.onmicrosoft.com>
2020-07-28 07:45:35 +0000

    Updated Update-AzLandingZone.ps1

--- runbooks/Update-AzLandingZone.ps1
+++ runbooks/Update-AzLandingZone.ps1
@@ -1,3 +1,31 @@
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+
+    "Logging in to Azure..."
+    $connectionResult =  Connect-AzAccount -Tenant $servicePrincipalConnection.TenantID `
+                             -ApplicationId $servicePrincipalConnection.ApplicationID   `
+                             -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint `
+                             -ServicePrincipal
+    "Logged in."
+    
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+    return
+} 
+
+
     #
     # External resource required
     #

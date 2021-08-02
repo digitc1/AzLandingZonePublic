4d07ee7c68db7bdbcf8861ae665f978a0b9a3993
augustin <augustin.colle@accenture.com>
2021-08-02 09:15:32 +0200

    update Azure automation runbook

--- runbooks/Update-AzLandingZone.ps1
+++ runbooks/Update-AzLandingZone.ps1
@@ -1,22 +1,21 @@
 $connectionName = "AzureRunAsConnection"
-try
-{
+try {
     # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
 
     "Logging in to Azure..."
-    Add-AzureRmAccount `
+    Add-AzAccount `
         -ServicePrincipal `
         -TenantId $servicePrincipalConnection.TenantId `
         -ApplicationId $servicePrincipalConnection.ApplicationId `
         -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
 }
 catch {
-    if (!$servicePrincipalConnection)
-    {
+    if (!$servicePrincipalConnection) {
         $ErrorMessage = "Connection $connectionName not found."
         throw $ErrorMessage
-    } else{
+    }
+    else {
         Write-Error -Message $_.Exception
         throw $_.Exception
     }
@@ -41,15 +40,15 @@ $name = Get-AutomationVariable -Name "name"
 $name
 $managementGroupName = Get-AutomationVariable -Name "managementGroupName"
 $managementGroupName
-if (!( $GetResourceGroup = Get-AzureRmResourceGroup | Where-Object {$_.ResourceGroupName -Like "*$name*"} )) {
+if (!( $GetResourceGroup = Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -Like "*$name*" } )) {
     "No Resource Group for Secure Landing Zone found"
     return 1;
 }
-if (!( $GetStorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $GetResourceGroup.ResourceGroupName | Where-Object { $_.StorageAccountName -Like "*$name*" } )) {
+if (!( $GetStorageAccount = Get-AzStorageAccount -ResourceGroupName $GetResourceGroup.ResourceGroupName | Where-Object { $_.StorageAccountName -Like "*$name*" } )) {
     "No Storage Account found for Secure Landing Zone"
     return 1;
 }
-if (!($GetManagementGroup = Get-AzureRmManagementGroup -Expand -GroupName $managementGroupName )) {
+if (!($GetManagementGroup = Get-AzManagementGroup -Expand -GroupName $managementGroupName )) {
     Write-Host "No Management group found for Secure Landing Zone"
     return 1;
 }
@@ -60,31 +59,31 @@ $scope = ($GetManagementGroup).Id
 # Creating policy definition related to Azure Security Center
 #
 "Checking registration for Azure Security Center CIS Benchmark"
-if (!( Get-AzureRmPolicyAssignment -Scope $scope -Name "ASC_Default" )) {
+if (!( Get-AzPolicyAssignment -Scope $scope -Name "ASC_Default" )) {
     "Enabling first monitoring in Azure Security Center"
-    $Policy = Get-AzureRmPolicySetDefinition | Where-Object { $_.Properties.displayName -EQ 'Enable Monitoring in Azure Security Center' }
-    New-AzureRmPolicyAssignment -Name "ASC_Default" -DisplayName "Azure Security Center - Default" -PolicySetDefinition $Policy -Scope $scope | Out-Null
+    $Policy = Get-AzPolicySetDefinition | Where-Object { $_.Properties.displayName -EQ 'Enable Monitoring in Azure Security Center' }
+    New-AzPolicyAssignment -Name "ASC_Default" -DisplayName "Azure Security Center - Default" -PolicySetDefinition $Policy -Scope $scope | Out-Null
 }
 "Checking registration for extended Azure Security Center CIS Benchmark"
-if (!( Get-AzureRmPolicyAssignment -Scope $scope -Name "ASC_CIS" )) {
+if (!( Get-AzPolicyAssignment -Scope $scope -Name "ASC_CIS" )) {
     "Enabling second monitoring in Azure Security Center"
-    $Policy = Get-AzureRmPolicySetDefinition | Where-Object { $_.Properties.displayName -EQ 'CIS Microsoft Azure Foundations Benchmark 1.1.0' }
-    New-AzureRmPolicyAssignment -Name "ASC_CIS" -DisplayName "Azure Security Center - CIS Compliance" -PolicySetDefinition $Policy -Scope $scope -listOfRegionsWhereNetworkWatcherShouldBeEnabled $location | Out-Null
+    $Policy = Get-AzPolicySetDefinition | Where-Object { $_.Properties.displayName -EQ 'CIS Microsoft Azure Foundations Benchmark 1.1.0' }
+    New-AzPolicyAssignment -Name "ASC_CIS" -DisplayName "Azure Security Center - CIS Compliance" -PolicySetDefinition $Policy -Scope $scope -listOfRegionsWhereNetworkWatcherShouldBeEnabled $location | Out-Null
 }
 "Checking policy for Azure Security Center coverage"
-if (!(Get-AzureRmPolicyAssignment -Scope $scope -Name "SLZ-SCCoverage" )) {
+if (!(Get-AzPolicyAssignment -Scope $scope -Name "SLZ-SCCoverage" )) {
     "Enabling Azure Security Center coverage"
     Invoke-WebRequest -Uri $definitionSecurityCenterCoverage -OutFile $HOME/rule.json
-    $policyDefinition = New-AzureRmPolicyDefinition -Name "SLZ-SCCoverage" -Policy $HOME/rule.json -ManagementGroupName $GetManagementGroup.Name
-    New-AzureRmPolicyAssignment -name "SLZ-SCCoverage" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location $location | Out-Null
+    $policyDefinition = New-AzPolicyDefinition -Name "SLZ-SCCoverage" -Policy $HOME/rule.json -ManagementGroupName $GetManagementGroup.Name
+    New-AzPolicyAssignment -name "SLZ-SCCoverage" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location $location | Out-Null
     Remove-Item -Path $HOME/rule.json
 }
 "Checking policy for Azure Security Center Auto-provisioning agents"
-if (!(Get-AzureRmPolicyAssignment -Scope $scope -Name "SLZ-SCAutoProvisioning" )) {
+if (!(Get-AzPolicyAssignment -Scope $scope -Name "SLZ-SCAutoProvisioning" )) {
     "Enabling Azure Security Center auto-provisioning"
     Invoke-WebRequest -Uri $definitionSecurityCenterAutoProvisioning -OutFile $HOME/rule.json
-    $policyDefinition = New-AzureRmPolicyDefinition -Name "SLZ-SCAutoProvisioning" -Policy $HOME/rule.json -ManagementGroupName $GetManagementGroup.Name
-    New-AzureRmPolicyAssignment -name "SLZ-SCAutoProvisioning" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location $location | Out-Null
+    $policyDefinition = New-AzPolicyDefinition -Name "SLZ-SCAutoProvisioning" -Policy $HOME/rule.json -ManagementGroupName $GetManagementGroup.Name
+    New-AzPolicyAssignment -name "SLZ-SCAutoProvisioning" -PolicyDefinition $policyDefinition -Scope $scope -AssignIdentity -Location $location | Out-Null
     Remove-Item -Path $HOME/rule.json
 }
 
@@ -100,7 +99,7 @@ Get-Content -Path $HOME/definitionList.txt | ForEAch-Object {
 
     "Checking policy : $policyName"
 
-    if ($GetPolicyInitiative = Get-AzureRmPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name "SLZ-policyGroup1" ) {
+    if ($GetPolicyInitiative = Get-AzPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name "SLZ-policyGroup1" ) {
         if ($tmp = $GetPolicyInitiative.Properties.PolicyDefinitions.Where( { $_.PolicyDefinitionId -Like "*$policyName" }, 'SkipUntil', 1)) {
             $effect = $tmp.parameters.effect.value
         }
@@ -113,12 +112,12 @@ Get-Content -Path $HOME/definitionList.txt | ForEAch-Object {
     }
     $param = @{ storageAccountId = @{value = $GetStorageAccount.Id }; region = @{value = $GetResourceGroup.Location }; effect = @{value = $effect } }
 
-    $policyDefinition = Get-AzureRmPolicyDefinition -ManagementGroupName $GetManagementGroup.Name | Where-Object { $_.Name -like $policyName }
+    $policyDefinition = Get-AzPolicyDefinition -ManagementGroupName $GetManagementGroup.Name | Where-Object { $_.Name -like $policyName }
     if ($policyDefinition) {
         if (!($policyDefinition.Properties.metadata.version -eq $policyVersion)) {
             Invoke-WebRequest -Uri $policyLink -OutFile $HOME/$policyName.json
             $metadata = '{"version":"' + $policyVersion + '"}'
-            $policyDefinition = Set-AzureRmPolicyDefinition -Id $policyDefinition.ResourceId -Policy $HOME/$policyName.json -Metadata $metadata
+            $policyDefinition = Set-AzPolicyDefinition -Id $policyDefinition.ResourceId -Policy $HOME/$policyName.json -Metadata $metadata
             Remove-Item -Path $HOME/$policyName.json
             "Updated policy: $policyName"
         }
@@ -126,18 +125,18 @@ Get-Content -Path $HOME/definitionList.txt | ForEAch-Object {
     else {
         Invoke-WebRequest -Uri $policyLink -OutFile $HOME/$policyName.json
         $metadata = '{"version":"' + $policyVersion + '"}'
-        $policyDefinition = New-AzureRmPolicyDefinition -Name $policyName -Policy $HOME/$policyName.json -Parameter $HOME/parameters.json -Metadata $metadata -ManagementGroupName $GetManagementGroup.Name
+        $policyDefinition = New-AzPolicyDefinition -Name $policyName -Policy $HOME/$policyName.json -Parameter $HOME/parameters.json -Metadata $metadata -ManagementGroupName $GetManagementGroup.Name
         Remove-Item -Path $HOME/$policyName.json
         "Created policy: $policyName"
     }
     $definitionList += @{policyDefinitionId = $policyDefinition.ResourceId; parameters = $param }
 }
-Set-AzureRmPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name $GetPolicyInitiative.Name -PolicyDefinition ($definitionList | ConvertTo-Json -Depth 5) | Out-Null
+Set-AzPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name $GetPolicyInitiative.Name -PolicyDefinition ($definitionList | ConvertTo-Json -Depth 5) | Out-Null
 Remove-Item -Path $HOME/parameters.json
 Remove-Item -Path $HOME/definitionList.txt
 
 # Loop to create all "SLZ-...........DiagnosticToLogAnalytics" policies if log analytics workspace exists
-if ($GetLogAnalyticsWorkspace = Get-AzureRmOperationalInsightsWorkspace -ResourceGroupName $GetResourceGroup.ResourceGroupName) {
+if ($GetLogAnalyticsWorkspace = Get-AzOperationalInsightsWorkspace -ResourceGroupName $GetResourceGroup.ResourceGroupName) {
     Invoke-WebRequest -Uri $definitionParametersv2URI -OutFile $HOME/parameters.json
     Invoke-WebRequest -Uri $definitionListv2URI -OutFile $HOME/definitionList.txt
     $definitionList = @()
@@ -148,7 +147,7 @@ if ($GetLogAnalyticsWorkspace = Get-AzureRmOperationalInsightsWorkspace -Resourc
         $policyLink = $_.Split(',')[2]
 
         "Checking policy : $policyName"
-        if ( $GetPolicyInitiative = Get-AzureRmPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name "SLZ-policyGroup2" ) {
+        if ( $GetPolicyInitiative = Get-AzPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name "SLZ-policyGroup2" ) {
             if ($tmp = $GetPolicyInitiative.Properties.PolicyDefinitions.Where( { $_.PolicyDefinitionId -Like "*$policyName" }, 'SkipUntil', 1)) {
                 $effect = $tmp.parameters.effect.value
             }
@@ -161,12 +160,12 @@ if ($GetLogAnalyticsWorkspace = Get-AzureRmOperationalInsightsWorkspace -Resourc
         }
         $param = @{ workspaceId = @{value = $GetLogAnalyticsWorkspace.ResourceId }; region = @{value = $GetResourceGroup.Location }; effect = @{value = $effect } }
 
-        $policyDefinition = Get-AzureRmPolicyDefinition -ManagementGroupName $GetManagementGroup.Name -Name $policyName -ErrorAction Continue
+        $policyDefinition = Get-AzPolicyDefinition -ManagementGroupName $GetManagementGroup.Name -Name $policyName -ErrorAction Continue
         if ($policyDefinition) {
             if (!($policyDefinition.Properties.metadata.version -eq $policyVersion)) {
                 Invoke-WebRequest -Uri $policyLink -OutFile $HOME/$policyName.json
                 $metadata = '{"version":"' + $policyVersion + '"}'
-                $policyDefinition = Set-AzureRmPolicyDefinition -Id $policyDefinition.ResourceId -Policy $HOME/$policyName.json -Metadata $metadata
+                $policyDefinition = Set-AzPolicyDefinition -Id $policyDefinition.ResourceId -Policy $HOME/$policyName.json -Metadata $metadata
                 Remove-Item -Path $HOME/$policyName.json
                 "Updated : $policyName"
             }
@@ -174,20 +173,20 @@ if ($GetLogAnalyticsWorkspace = Get-AzureRmOperationalInsightsWorkspace -Resourc
         else {
             Invoke-WebRequest -Uri $policyLink -OutFile $HOME/$policyName.json
             $metadata = '{"version":"' + $policyVersion + '"}'
-            $policyDefinition = New-AzureRmPolicyDefinition -Name $policyName -Policy $HOME/$policyName.json -Parameter $HOME/parameters.json -Metadata $metadata -ManagementGroupName $GetManagementGroup.Name
+            $policyDefinition = New-AzPolicyDefinition -Name $policyName -Policy $HOME/$policyName.json -Parameter $HOME/parameters.json -Metadata $metadata -ManagementGroupName $GetManagementGroup.Name
             Remove-Item -Path $HOME/$policyName.json
             "Created : $policyName"
         }
         $definitionList += @{policyDefinitionId = $policyDefinition.ResourceId; parameters = $param }
     }
-    Set-AzureRmPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name $GetPolicyInitiative.Name -PolicyDefinition ($definitionList | ConvertTo-Json -Depth 5) | Out-Null
+    Set-AzPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name $GetPolicyInitiative.Name -PolicyDefinition ($definitionList | ConvertTo-Json -Depth 5) | Out-Null
     Remove-Item -Path $HOME/parameters.json
     Remove-Item -Path $HOME/definitionList.txt
 }
 
 # Loop to create all "SLZ-...........DiagnosticToEventHub" policies
-if ($GetEventHubNamespace = Get-AzureRmEventHubNamespace -ResourceGroupName $GetResourceGroup.ResourceGroupName) {
-    $GetEventHubAuthorizationRuleId = Get-AzureRmEventHubAuthorizationRule -ResourceGroupName $GetResourceGroup.ResourceGroupName -Namespace $GetEventHubNamespace.Name -Name "landingZoneAccessKey"
+if ($GetEventHubNamespace = Get-AzEventHubNamespace -ResourceGroupName $GetResourceGroup.ResourceGroupName) {
+    $GetEventHubAuthorizationRuleId = Get-AzEventHubAuthorizationRule -ResourceGroupName $GetResourceGroup.ResourceGroupName -Namespace $GetEventHubNamespace.Name -Name "landingZoneAccessKey"
     Invoke-WebRequest -Uri $definitionParametersv3URI -OutFile $HOME/parameters.json
     Invoke-WebRequest -Uri $definitionListv3URI -OutFile $HOME/definitionList.txt
     $definitionList = @()
@@ -198,7 +197,7 @@ if ($GetEventHubNamespace = Get-AzureRmEventHubNamespace -ResourceGroupName $Get
         $policyLink = $_.Split(',')[2]
 
         "Checking policy : $policyName"
-        if ($GetPolicyInitiative = Get-AzureRmPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name "SLZ-policyGroup3" ) {
+        if ($GetPolicyInitiative = Get-AzPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name "SLZ-policyGroup3" ) {
             if ($tmp = $GetPolicyInitiative.Properties.PolicyDefinitions.Where( { $_.PolicyDefinitionId -Like "*$policyName" }, 'SkipUntil', 1)) {
                 $effect = $tmp.parameters.effect.value
             }
@@ -211,12 +210,12 @@ if ($GetEventHubNamespace = Get-AzureRmEventHubNamespace -ResourceGroupName $Get
         }
         $param = @{ eventHubRuleId = @{value = $GetEventHubAuthorizationRuleId.Id }; region = @{value = $GetResourceGroup.Location }; effect = @{value = $effect }; eventHubName = @{value = "insights-operational-logs" } }
 
-        $policyDefinition = Get-AzureRmPolicyDefinition -ManagementGroupName $GetManagementGroup.Name -Name $policyName -ErrorAction Continue
+        $policyDefinition = Get-AzPolicyDefinition -ManagementGroupName $GetManagementGroup.Name -Name $policyName -ErrorAction Continue
         if ($policyDefinition) {
             if (!($policyDefinition.Properties.metadata.version -eq $policyVersion)) {
                 Invoke-WebRequest -Uri $policyLink -OutFile $HOME/$policyName.json
                 $metadata = '{"version":"' + $policyVersion + '"}'
-                $policyDefinition = Set-AzureRmPolicyDefinition -Id $policyDefinition.ResourceId -Policy $HOME/$policyName.json -Metadata $metadata
+                $policyDefinition = Set-AzPolicyDefinition -Id $policyDefinition.ResourceId -Policy $HOME/$policyName.json -Metadata $metadata
                 Remove-Item -Path $HOME/$policyName.json
                 "Updated : $policyName"
             }
@@ -224,17 +223,17 @@ if ($GetEventHubNamespace = Get-AzureRmEventHubNamespace -ResourceGroupName $Get
         else {
             Invoke-WebRequest -Uri $policyLink -OutFile $HOME/$policyName.json
             $metadata = '{"version":"' + $policyVersion + '"}'
-            $policyDefinition = New-AzureRmPolicyDefinition -Name $policyName -Policy $HOME/$policyName.json -Parameter $HOME/parameters.json -Metadata $metadata -ManagementGroupName $GetManagementGroup.Name
+            $policyDefinition = New-AzPolicyDefinition -Name $policyName -Policy $HOME/$policyName.json -Parameter $HOME/parameters.json -Metadata $metadata -ManagementGroupName $GetManagementGroup.Name
             Remove-Item -Path $HOME/$policyName.json
             "Created : $policyName"
         }
         $definitionList += @{policyDefinitionId = $policyDefinition.ResourceId; parameters = $param }
     }
-    Set-AzureRmPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name $GetPolicyInitiative.Name -PolicyDefinition ($definitionList | ConvertTo-Json -Depth 5) | Out-Null
+    Set-AzPolicySetDefinition -ManagementGroupName $GetManagementGroup.Name -Name $GetPolicyInitiative.Name -PolicyDefinition ($definitionList | ConvertTo-Json -Depth 5) | Out-Null
     Remove-Item -Path $HOME/parameters.json
     Remove-Item -Path $HOME/definitionList.txt
 	
-	if($GetLogAnalyticsWorkspace){
+    if ($GetLogAnalyticsWorkspace) {
         Invoke-WebRequest -Uri "https://raw.githubusercontent.com/digitc1/AzLandingZonePublic/develop/definitions/sentinel/hunting/definitionList.txt" -Method Get -OutFile "$HOME/definitionList.txt"
         Get-Content "$HOME/definitionList.txt" | ForEach-Object {
             $definitionId = $_.Split(',')[0]
@@ -244,23 +243,22 @@ if ($GetEventHubNamespace = Get-AzureRmEventHubNamespace -ResourceGroupName $Get
             $query = (Invoke-WebRequest -Uri $definitionUri -Method Get -UseBasicParsing).Content
             $body = [PSCustomObject]@{
                 'properties' = @{
-                    'Category' = 'Hunting Queries'
+                    'Category'    = 'Hunting Queries'
                     'DisplayName' = $definitionName 
-                    'Query' = $query
+                    'Query'       = $query
                 }
             }
             $uri = "https://management.azure.com" + $GetWorkspace.ResourceId + "/savedSearches/" + $definitionId + "?api-version=2020-08-01"
-            try{
+            try {
                 Write-Host -ForegroundColor Green "Creating custom hunting query: "$definitionName
                 $auth = Get-LzAccessToken
                 $requestResult = Invoke-webrequest -Uri $uri -Method Put -Headers $auth -Body ($body | ConvertTo-Json -Depth 5)
                 Write-Host -ForegroundColor Green "Created custom hunting query: "$definitionName
             }
             catch {
-                Switch ($_.Exception.Response.StatusCode.value__)
-                {
-                    409 {Write-Host "Connection already exists" -ForegroundColor Yellow}
-                    default {Write-Host "An unexpected error happened. Contact Landing Zone FMB for additional support." -ForegroundColor Red}
+                Switch ($_.Exception.Response.StatusCode.value__) {
+                    409 { Write-Host "Connection already exists" -ForegroundColor Yellow }
+                    default { Write-Host "An unexpected error happened. Contact Landing Zone FMB for additional support." -ForegroundColor Red }
                 }
             }
         }
